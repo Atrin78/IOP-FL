b5bb5dd6c6a3dfdaa577d6d39ea89572565274b3
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2024-03-17 18:53:28 -0700

    XFDG

--- nets/attention_unet.py
+++ nets/attention_unet.py
@@ -15,21 +15,21 @@ class Attention_block(nn.Module):
         super(Attention_block,self).__init__()
         self.W_g = nn.Sequential(
             OrderedDict([
-            ('0', RouteConv2D(F_g, F_int, name='0', kernel_size=1,stride=1,padding=0,bias=True)),
+            ('0', RouteConv2D(F_g, F_int, name=name+'.W_g'+'.0', kernel_size=1,stride=1,padding=0,bias=True)),
             ('1', nn.BatchNorm2d(F_int))
             ])
             )
         
         self.W_x = nn.Sequential(
             OrderedDict([
-            ('0', RouteConv2D(F_l, F_int, name='0', kernel_size=1,stride=1,padding=0,bias=True)),
+            ('0', RouteConv2D(F_l, F_int, name=name+'.W_x'+'.0', kernel_size=1,stride=1,padding=0,bias=True)),
             ('1', nn.BatchNorm2d(F_int))
         ])
         )
 
         self.psi = nn.Sequential(
             OrderedDict([
-            ('0', RouteConv2D(F_int, 1, name='0', kernel_size=1,stride=1,padding=0,bias=True)),
+            ('0', RouteConv2D(F_int, 1, name=name+'.W_psi'+'.0', kernel_size=1,stride=1,padding=0,bias=True)),
             ('1', nn.BatchNorm2d(1)),
             ('2', nn.Sigmoid())
         ])
